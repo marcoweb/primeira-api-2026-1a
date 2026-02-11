74cbf051c838a1c48804ec3d93d090f80d5202d5
Marco Antônio Gusmão Carvalho <marcocarvalho.web@gmail.com>
2026-02-11 17:28:43 +0000

    Primeira Rota

---
+++ src/main/java/app/controllers/TarefasController.java
@@ -0,0 +1,15 @@
+package app.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/tarefas")
+public class TarefasController {
+    @GetMapping
+    public String list() {
+        return "Olá Mundo!!!";
+    }
+}
