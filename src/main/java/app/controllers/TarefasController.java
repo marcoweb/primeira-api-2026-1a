364a0c9e933e2898dd74293aa7ea8253425a5e04
Marco Antônio Gusmão Carvalho <marcocarvalho.web@gmail.com>
2026-02-23 19:17:14 +0000

    Aula 23/02/2026

--- src/main/java/app/controllers/TarefasController.java
+++ src/main/java/app/controllers/TarefasController.java
@@ -6,18 +6,49 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
 import app.model.Tarefa;
+import app.repository.TarefaRepository;
+
+import java.util.Optional;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController
 @RequestMapping("/tarefas")
 public class TarefasController {
+    @Autowired
+    private TarefaRepository tarefaRepo;
+
     @GetMapping
-    public Tarefa list() {
-        return new Tarefa(1, "Teste");
+    public Iterable<Tarefa> list() {
+        return tarefaRepo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Tarefa getOne(@PathVariable long id) {
+        return tarefaRepo.findById(id).get();
     }
 
     @PostMapping
-    public Tarefa insert(@RequestBody Tarefa nova) {
-        return nova;
+    public Tarefa insert(@RequestBody Tarefa novaTarefa) {
+        return tarefaRepo.save(novaTarefa);
+    }
+
+    @PutMapping("/{id}")
+    public Tarefa update(@PathVariable long id, @RequestBody Tarefa modif){
+        Optional<Tarefa> busca = tarefaRepo.findById(id);
+        busca.get().setDescricao(modif.getDescricao());
+        return tarefaRepo.save(busca.get());
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        tarefaRepo.deleteById(id);
     }
 }
--- src/main/java/app/model/Tarefa.java
+++ src/main/java/app/model/Tarefa.java
@@ -2,10 +2,22 @@ package app.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@Entity
+@Table(name="tarefas")
 public class Tarefa {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private long id;
     private String descricao;
 
